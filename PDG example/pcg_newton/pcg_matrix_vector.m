be0fdc184c4bbb774437027339cfffb76c60fe23
David <40886462+DayouLuo1995@users.noreply.github.com>
2024-12-09 14:16:25 -0800

    name changes

--- PDG example/pcg_newton/pcg_matrix_vector.m
+++
@@ -1,8 +0,0 @@
-function out = pcg_matrix_vector(A,Q,proj_index_all, nx, nu, ne,Jk, deltaY,alpha_p, beta_p,x0)
-%J_K'*H * SInv*J_D*H' j_k + (I + deltaY - jk)
-
-temp = dev_g_Q_multi_vector(Q, dev_g_Q_multi_vector(Q, dev_HtZ_g(A,Jk.*x0, ne), proj_index_all, nx, nu, true) ...
-    ,proj_index_all, nx, nu, false);
-out = (alpha_p*beta_p)*Jk.*dev_HZ_g(A, temp, ne) + ((1 + deltaY)*ones(size(Jk)) - Jk) .* x0;
-end
-
--- PDG example/pcg_newton/pcg_solve_precondition.m
+++
@@ -1,3 +0,0 @@
-function out = pcg_solve_precondition(L, x)
-out = dev_solve_LTv(L, dev_solve_Lv(L, x));
-end
